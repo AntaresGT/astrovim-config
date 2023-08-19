fd36253012df3c2c3c7af2ce188f4097a3a90076
AntaresGT <98500646+AntaresGT@users.noreply.github.com>
2023-08-19 19:21:36 +0200

    Commit inicial

---
+++ lua/user/init.lua
@@ -0,0 +1,15 @@
+
+
+return {
+    mappings = {
+        -- Mapeo personalizado para teclados en español
+        i = {
+            ["<C-,>"] = { "<" },
+            ["<C-.>"] = { ">" },
+            ["<C-1>"] = { "~" },
+            ["<C-2>"] = { "\\" },
+            ["<C-3>"] = { "^" },
+            ["<C-4>"] = { "`" },
+        }
+    }
+}
